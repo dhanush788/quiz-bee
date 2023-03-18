d502ea608d17fb1e9ce7fae5e24c5439ffd9f50c
dhanush788 <dhanushpk50@gmail.com>
2023-03-18 15:55:12 +0530

    Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages';
 import Video from './pages/video';
 import Quiz from './pages/quiz';
